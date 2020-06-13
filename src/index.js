dbc861adc54feb1b523599118afc95c892ea02af
Alexispoveda <alexis.mars20@hotmail.com>
2020-06-13 11:58:38 -0500

    Install react mdl

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
